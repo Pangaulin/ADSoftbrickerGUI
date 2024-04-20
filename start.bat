de89030fffefe1ccb291862615fb797e69d01042
Pangaulin <contact.pangaulin@gmail.com>
2024-04-20 11:44:07 +0200

    install requirements automatically

--- start.bat
+++ start.bat
@@ -1,2 +1,14 @@
 chcp 65001
+@echo off
+setlocal
+
+set "file=first_run.txt"
+
+for /f %%i in (%file%) do set content=%%i
+if "%content%"=="1" (
+pip install -r requirements.txt
+echo 0 > %file%
+)
+endlocal
+
 python main.py
